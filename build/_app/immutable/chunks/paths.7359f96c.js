3d5cfc6474964e292971574f1a6d19ec72d218dd
3bitdiy <stevan@3bit.diy>
2026-02-27 16:11:36 +0100

    Remove generated build artifacts
    Delete the build/ directory and its generated immutable assets (static images, CSS, HTML). This removes build/200.html and many files under build/_app/immutable/assets to clean up committed build output and reduce repository size.

--- build/_app/immutable/chunks/paths.7359f96c.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1mog1ef)==null?void 0:s.base)??"/saife-toolkit";var e;const a=((e=globalThis.__sveltekit_1mog1ef)==null?void 0:e.assets)??t;export{a,t as b};
