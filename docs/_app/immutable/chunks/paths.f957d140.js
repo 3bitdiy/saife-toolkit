2224bfca96d83f38cb2c8e7e964205132cd1daf4
3bitdiy <stevan@3bit.diy>
2026-03-13 07:15:09 +0100

    Test Hanken Grotesk font

---
+++ docs/_app/immutable/chunks/paths.f957d140.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_bpghas)==null?void 0:s.base)??"/saife-toolkit";var a;const e=((a=globalThis.__sveltekit_bpghas)==null?void 0:a.assets)??t;export{e as a,t as b};
